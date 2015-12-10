8388f975af12f4e24c044f18efd73716c2e6e493
yulining <yulining@hu-berlin.de>
2015-12-10 09:57:55 +0100

    total in out groups

--- TENET_total_in_out_groups/TENET_total_in_out_groups.R
+++ TENET_total_in_out_groups/TENET_total_in_out_groups.R
@@ -2,11 +2,10 @@
 rm(list = ls(all = TRUE))
 graphics.off()
 
-# set the working directory
-#setwd("C:/...")
+# set the working directory setwd('C:/...')
 
 f            = read.csv("total_in_and_out.csv")
-dt           = as.Date(f[,1], format = "%d/%m/%Y")
+dt           = as.Date(f[, 1], format = "%d/%m/%Y")
 
 # total incoming links of four groups
 total.in.b   = as.vector(f[, 2])
@@ -15,7 +14,8 @@ total.in.d   = as.vector(f[, 4])
 total.in.o   = as.vector(f[, 5])
 
 # banks with incoming links
-plot(dt, total.in.b, type = "l", col = "white", lwd = 5, ylab = "", xlab = "", cex.axis = 1.8, font.axis = 1.8, ylim = range(0,25))
+plot(dt, total.in.b, type = "l", col = "white", lwd = 5, ylab = "", xlab = "", cex.axis = 1.8, 
+  font.axis = 1.8, ylim = range(0, 25))
 lines(smooth.spline(dt, total.in.b, spar = 0.5), lwd = 5, col = "red")
 
 # insurers with incoming links
@@ -35,15 +35,15 @@ total.out.o   = as.vector(f[, 9])
 
 # banks with outgoing links
 dev.new()
-plot(dt, total.out.b, type = "l", col = "white", lwd = 5, ylab = "", xlab = "", cex.axis = 1.8, font.axis = 1.8, ylim = range(0,25))
+plot(dt, total.out.b, type = "l", col = "white", lwd = 5, ylab = "", xlab = "", cex.axis = 1.8, 
+  font.axis = 1.8, ylim = range(0, 25))
 lines(smooth.spline(dt, total.out.b, spar = 0.5), lwd = 5, col = "red")
 
 # insurers with outgoing links
 lines(smooth.spline(dt, total.out.ins, spar = 0.5), lty = 2, col = "blue", lwd = 5)
 
-
 # broker dealers with outgoing links
 lines(smooth.spline(dt, total.out.d, spar = 0.5), lty = 3, col = "green4", lwd = 5)
 
 # others with outgoing links
-lines(smooth.spline(dt, total.out.o, spar = 0.5), lty = 4, col = "purple", lwd = 5)
+lines(smooth.spline(dt, total.out.o, spar = 0.5), lty = 4, col = "purple", lwd = 5) 
