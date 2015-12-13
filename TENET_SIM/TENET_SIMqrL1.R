6359866631b25ad514798b3040e44568dacd35a1
Lining Yu <yulining@hu-berlin.de>
2015-12-13 20:28:07 +0100

    TENET_SIM

---
+++ TENET_SIM/TENET_SIMqrL1.R
@@ -0,0 +1,110 @@
+sim = function(Qy, Qxx, Qp, Qmaxiter, Qi = l, Qj = k, LVaRest) {
+  # local polynomial regression quantile which is used in the iteration
+  lprq0 = function(x, y, h, tau, x0) {
+    require(quantreg)
+    fv = x0
+    dv = x0
+    z  = x - x0
+    wx = dnorm(z/h)
+    r  = rq(y ~ z, weights = wx, tau = tau, ci = FALSE)
+    fv = r$coef[1]
+    dv = r$coef[2]
+    list(x0 = x0, fv = fv, dv = dv)
+  }
+  
+  # set the critical value to control the iteration
+  crit  = 0.01
+  
+  fit   = qrL1(Qxx, Qy, Qp, 50)
+  # choose the number which has the minimal value of generalized approximate cross
+  # validation criterion
+  isnum = which(fit$Cgacv == min(fit$Cgacv))
+  # the initial estimated betas
+  beta.in   = fit$beta[isnum, ]
+  # the initial estimated penalization parameter
+  lambda_in = (-fit$lambda[isnum])
+  print(lambda_in)
+  d = length(beta.in)
+  n = NROW(Qy)
+  print(which(beta.in != 0))
+  # standardize the initial betas
+  beta.in  = beta.in/sqrt(sum(beta.in^2))
+  # initialize the new estimated betas in the iterations
+  beta.new = rep(0, d)
+  # initialize the number of iteration
+  iter = 1
+  # initialize the link function 'a' and its first derivative 'b'
+  a.in = rep(0, n)
+  b.in = rep(0, n)
+  
+  # start the iteration
+  while ((iter < Qmaxiter) & (sum((beta.new - beta.in)^2) > crit)) {
+    print(iter)
+    if (iter > 1) {
+      beta.in = beta.new
+    }
+    # step 1: compute a.in and b.in
+    index_x = rowSums(t(t(Qxx) * beta.in))
+    yorder  = Qy[order(index_x)]
+    xorder  = sort(index_x)
+    hm = dpill(xorder[2:(length(index_x) - 2)], yorder[2:(length(index_x) - 2)])
+    hm[is.na(hm)] = 0.08
+    hp = 10 * hm * (Qp * (1 - Qp)/(dnorm(qnorm(Qp)))^2)^0.2
+    x0 = 0
+    for (j in 1:n) {
+      x0  = index_x[j]
+      fit = lprq0(index_x, Qy, hp, Qp, x0)
+      a.in[j] = fit$fv
+      b.in[j] = fit$dv
+    }
+    # step 2: compute beta.new
+    ynew = rep(0, n^2)
+    xnew = rep(0, n^2 * d)
+    xnew = matrix(xnew, ncol = d)
+    ynew = (rep(Qy, each = n) - rep(a.in, times = n))
+    xnew = rep(b.in, times = n) * (Qxx[rep(1:n, each = n), ] - Qxx[rep(1:n, 
+    times = n), ])
+    wts  = dnorm(rowSums(t(t(xnew) * beta.in))/hp)
+    ynew = sqrt(wts) * ynew
+    wtss = replicate(d, wts)
+    xnew = xnew * sqrt(wtss)
+    fit  = qrL1(xnew, ynew, Qp, 200)
+    # choose the number which minimizes Bayesian information criterion (BIC, or
+    # SIC)
+    isnum      = which(fit$Csic == min(fit$Csic))
+    beta.new   = fit$beta[isnum, ]
+    beta.new   = beta.new/sqrt(sum(beta.new^2))
+    lambda_new = (-fit$lambda[isnum])
+    print(lambda_new)
+    iter       = iter + 1
+  }
+  
+  index_final  = rowSums(t(t(Qxx) * beta.new))
+  value_x      = seq(min(index_final), max(index_final), length = length(Qy))
+  linkfunest   = matrix(0, length(value_x), 1)
+  for (i in 1:length(value_x)) {
+    fit2       = lprq0(index_final, Qy, hp, Qp, value_x[i])
+    linkfunest[i] = fit2$fv
+  }
+  index_est = LVaRest %*% beta.new
+  fit1      = lprq0(index_final, Qy, hp, Qp, index_est)
+  # a.fi is the estimated g(), i.e. estimated CoVaR
+  a.fi = fit1$fv
+  # b.fi is the estimated g'()
+  b.fi = fit1$dv
+  # c.fi is the estimated betas multiply g'(), i.e. the estimated partial
+  # derivatives
+  c.fi = b.fi * beta.new
+  
+  finalresults            = list() 
+  finalresults$beta_final = beta.new
+  finalresults$lambda.fi  = lambda_new
+  finalresults$a.fi = a.fi
+  finalresults$b.fi = b.fi
+  finalresults$c.fi = c.fi
+  return(finalresults)
+}
+
+
+
+ 
---
+++ TENET_SIM/TENET_main_SIM.R
@@ -0,0 +1,76 @@
+# clear all variables
+rm(list = ls(all = TRUE))
+graphics.off()
+# set the working directory setwd('C:/...')
+# install and load packages
+libraries = c("quantreg", "KernSmooth", "SparseM", "MASS")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)
+})
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+source("TENET_SIMqrL1.r")
+source("quantilelasso.r")
+
+# read the file which includes log returns of 100 firms and 7 macro state
+# variables
+x0 = read.csv("100_firms_returns_and_macro_2015-04-15.csv", header = TRUE)
+# all firms' characteristics from balance sheet information of 100 firms
+Bal_sheet_full = read.csv(file = "Bal_sheet.csv")
+# 7 macro state variables
+m  = as.matrix(x0[, 102:108])
+# estimated Value at Risk of 100 firms
+VaR = as.matrix(read.csv("VaR_movingwindows_20150617.csv")[-1])[, 1:100]
+# log returns of 100 firms
+xx0 = x0[, 2:101]
+# start the linear quantile lasso estimation for each firm
+for (k in 1:100) {
+  cat("Firm:", k)
+  # log return of firm k
+  y   = as.matrix(xx0[, k])
+  # log returns of firms except firm k
+  xx1 = as.matrix(xx0[, -k])
+  # 4 firm characteristics from balance sheet informaiton of firm k
+  BS  = Bal_sheet_full[, (4 * k - 3):(4 * k)]
+  # combine macro state variables and 4 firm characteristics
+  MB  = cbind(m, BS)
+  # number of rows of log return
+  n   = nrow(xx1)
+  # number of covariates
+  p   = ncol(xx1) + ncol(MB)
+  # estimated Value at Risk of firms except firm k
+  V   = as.matrix(VaR[, -k])
+  # quantile level
+  tau = 0.05
+  # moving window size equals 48 corresponds to one year weekly data
+  ws  = 48
+  
+  lambda_sim  = matrix(0, (n - ws), 1)
+  beta_sim    = matrix(0, (n - ws), p)
+  covar_sim   = matrix(0, (n - ws), 1)
+  first_der   = matrix(0, (n - ws), 1)
+  partial_der = matrix(0, (n - ws), p)
+  
+  for (l in 1:(n - ws)) {
+    yw  = y[l:(l + ws)]
+    MBw = MB[l:(l + ws), ]
+    mb  = matrix(0, ws + 1, ncol(MB))
+    # standardize macro state variables and 4 firm characteristics
+    for (j in 1:ncol(MB)) {
+      mb[, j] = (MBw[, j] - min(MBw[, j]))/(max(MBw[, j]) - min(MBw[, j]))
+    }
+    mb[is.na(mb)]   = 0
+    MBw[is.na(MBw)] = 0
+    xx       = xx1[l:(l + ws), ]
+    # all the independent variables
+    xxw      = cbind(xx, mb)
+    VaRM_est = as.numeric(c(V[l, ], mb[(ws + 1), ]))
+    fit      = sim(yw, xxw, tau, Qmaxiter = 2, l, k, LVaRest = VaRM_est)
+    beta_sim[l, ]    = fit$beta_final
+    lambda_sim[l]    = fit$lambda.fi
+    # the final estimated CoVaR
+    covar_sim[l]     = fit$a.fi
+    first_der[l]     = fit$b.fi
+    # the estimated partial derivatives
+    partial_der[l, ] = fit$c.fi
+  }
+} 
